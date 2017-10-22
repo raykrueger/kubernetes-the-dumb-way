96c99b8f84012e1349b761d54bc724ab43f06dfa
raykrueger <raykrueger@gmail.com>
2017-10-21 19:42:27 -0500

    Working hosts, certs, and etcd

---
+++ gencerts.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env sh
+
+cat > ca-config.json <<EOF
+{
+  "signing": {
+    "default": {
+      "expiry": "8760h"
+    },
+    "profiles": {
+      "kubernetes": {
+        "usages": ["signing", "key encipherment", "server auth", "client auth"],
+        "expiry": "8760h"
+      }
+    }
+  }
+}
+EOF
+
+cat > ca-csr.json <<EOF
+{
+  "CN": "Kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "CA",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+cat > admin-csr.json <<EOF
+{
+  "CN": "admin",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:masters",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  admin-csr.json | cfssljson -bare admin
+
+for instance in worker-0 worker-1 worker-2; do
+cat > ${instance}-csr.json <<EOF
+{
+  "CN": "system:node:${instance}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:nodes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+EXTERNAL_IP=$(docker-machine ip ${instance})
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=${instance},${EXTERNAL_IP} \
+  -profile=kubernetes \
+  ${instance}-csr.json | cfssljson -bare ${instance}
+done
+
+cat > kube-proxy-csr.json <<EOF
+{
+  "CN": "system:kube-proxy",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "system:node-proxier",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kube-proxy-csr.json | cfssljson -bare kube-proxy
+
+cat > kubernetes-csr.json <<EOF
+{
+  "CN": "kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+#TODO Kelsey used a static IP for a loadbalancer in front of all the
+# controllers. I'll just add all the IP addresses for now.
+
+for instance in controller-0 controller-1 controller-2; do
+  KUBERNETES_PUBLIC_ADDRESS+="$(docker-machine ip ${instance}),"
+done
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${KUBERNETES_PUBLIC_ADDRESS}127.0.0.1,kubernetes.default \
+  -profile=kubernetes \
+  kubernetes-csr.json | cfssljson -bare kubernetes
---
+++ install-etcd.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+for instance in controller-0 controller-1 controller-2; do
+  INTERNAL_IP=$(docker-machine ip ${instance})
+  docker-machine ssh ${instance} sudo mkdir -p /etc/etcd/certs /var/lib/etcd
+  docker-machine ssh ${instance} sudo cp ca.pem kubernetes-key.pem kubernetes.pem /etc/etcd/certs/
+
+  docker-machine ssh ${instance} \
+    docker run -d -v /etc/etcd/certs:/etc/etcd/certs -v /var/lib/etcd:/var/lib/etcd -p 4001:4001 -p 2380:2380 -p 2379:2379 --name etcd quay.io/coreos/etcd:latest \
+    /usr/local/bin/etcd \
+    --name ${instance} \
+    --cert-file=/etc/etcd/certs/kubernetes.pem \
+    --key-file=/etc/etcd/certs/kubernetes-key.pem \
+    --peer-cert-file=/etc/etcd/certs/kubernetes.pem \
+    --peer-key-file=/etc/etcd/certs/kubernetes-key.pem \
+    --trusted-ca-file=/etc/etcd/certs/ca.pem \
+    --peer-trusted-ca-file=/etc/etcd/certs/ca.pem \
+    --peer-client-cert-auth \
+    --client-cert-auth \
+    --initial-advertise-peer-urls https://${INTERNAL_IP}:2380 \
+    --listen-peer-urls https://0.0.0.0:2380 \
+    --listen-client-urls https://0.0.0.0:2379 \
+    --advertise-client-urls https://${INTERNAL_IP}:2379 \
+    --initial-cluster-token etcd-cluster-0 \
+    --initial-cluster controller-0=https://$(docker-machine ip controller-0):2380,controller-1=https://$(docker-machine ip controller-1):2380,controller-2=https://$(docker-machine ip controller-2):2380 \
+    --initial-cluster-state new \
+    --data-dir=/var/lib/etcd
+done
