2b305cd9fc5c8082cf24e766c1f1819847162679
raykrueger <raykrueger@gmail.com>
2017-10-26 20:14:49 -0500

    Configure kubectl

---
+++ configure-kubectl.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+KUBERNETES_PUBLIC_ADDRESS=$(docker-machine ip controller-0)
+
+kubectl config set-cluster kubernetes-the-hard-way \
+  --certificate-authority=ca.pem \
+  --embed-certs=true \
+  --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443
+
+kubectl config set-cluster kubernetes-the-hard-way \
+  --certificate-authority=ca.pem \
+  --embed-certs=true \
+  --server=https://${KUBERNETES_PUBLIC_ADDRESS}:6443
+
+kubectl config set-credentials admin \
+  --client-certificate=admin.pem \
+  --client-key=admin-key.pem
+
+kubectl config set-context kubernetes-the-hard-way \
+  --cluster=kubernetes-the-hard-way \
+  --user=admin
+
+kubectl config use-context kubernetes-the-hard-way
+
+kubectl get componentstatuses,nodes
+
+echo "Hopefully you see healthy stuff above :)"
